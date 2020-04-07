e43f47b66cbc260ef6fe4fb1685398282a2b9e6c
tomo-2525 <tomoblog.0205@gmail.com>
2020-04-07 17:49:55 +0900

    update

--- js/main2.js
+++ js/main2.js
@@ -5,10 +5,16 @@ function showTask(selectedKey) {
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         var data = localStorage.getItem(key);
+
+        var taskData = data.split(':');
+
         if (selectedKey == key) {
             taskList.innerHTML
-                += '<input type="text" name="modTask" '
-                + ' size="30" value="' + data + ' " >'
+                += '<input type="range" name="modProgress" '
+                + ' min="0" max="100" step="5" '
+                + 'value="' + taskData[1] + '">'
+                + '<input type="text" name="modTask" '
+                + ' size="30" value="' + taskData[0] + ' " >'
                 + '<input type="button" value="タスクの修正"　'
                 + ' onclick="setTask(' + key + ')">'
                 + '<input type="button" value="タスクの削除"　'
@@ -16,8 +22,10 @@ function showTask(selectedKey) {
                 + '<br><br>';
         } else {
             taskList.innerHTML
-                += '<a href="#" onclick="showTask(' + key + ')">'
-                + '[' + data + ']</a>'
+                += '<meter min="0" max="100" '
+                + ' value="' + taskData[0] + '"></meter>'
+                + '<a href="#" onclick="showTask(' + key + ')">'
+                + '[' + taskData[0] + '(' + taskData[1] + '%)]</a>'
                 + '<br><br>';
         }
     }
@@ -27,10 +35,14 @@ function setTask(selectedKey) {
     if (selectedKey == 'new') {
         var key = new Date().getTime();
         var data = document.form1.newTask.value;
+        var progress = document.form1.newProgress.value;
     } else {
         var key = selectedKey;
         var data = document.form1.modTask.value;
+        var progress = document.form1.modProgress.value;
     }
+
+    var data = data + ':' + progress;
 
     localStorage.setItem(key, data);
     showTask();
