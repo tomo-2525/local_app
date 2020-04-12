f388c39244f8ade145453e88c42f1b79e702e3cf
tomo-2525 <tomoblog.0205@gmail.com>
2020-04-13 00:21:08 +0900

    update

---
+++ js/main4.js
@@ -0,0 +1,28 @@
+var addressList = document.getElementById("addressList");
+
+function showAddress() {
+    var db = openDatabase('addressDB', '1', 'AddressDB', '2*1024*1024');
+
+    db.transaction(function (tx) {
+        var qword = document.form1.qword.value;
+
+        sql = 'SELECT id, name, email FROM address_tb'
+            + 'WHERE name LIKE "%' + qword + '%"'
+            + 'OR email LIKE "%' + qword + '%"';
+
+        tx.executeSQL(sql, [], function (tx, rs) {
+            addressList.innerHTML = "";
+
+            for (var i = 0; i < rs.rows.length; i++) {
+                var rowText = '<tr>'
+                    + '<td>' + rs.rows.item(i).name + '</td>'
+                    + '<td>' + rs.rows.item(i).email + '</td>'
+                    + '</tr>';
+
+                addressList.innerHTML += rowText;
+            }
+        }, null);
+    });
+
+    return false;
+}
